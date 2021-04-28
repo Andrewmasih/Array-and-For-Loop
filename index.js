83b645ba35b9fe05f20bda0316951adad734bd44
Andrew Masih <armasih@outlook.com>
2021-04-28 22:02:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let fruit = ["apple", "pears", "banana", "oranages", "grapes", "kiwi", "pineapple", "mango", "strawberries", "cherries", "cucumber"];
+
+
+
+for (let i = 0; i < fruit.length; i++) {
+
+console.log (i);
+console.log (fruit);
+
+
+ 
+
+
+	
+
+}
+
+
+
