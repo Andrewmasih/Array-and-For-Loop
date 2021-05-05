d7b95b804157298c6824242db834bcee44a9a2a3
Andrew Masih <armasih@outlook.com>
2021-05-05 10:26:31 +0000

    added some text

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
+
 let fruit = ["apple", "pears", "banana", "oranages", "grapes", "kiwi", "pineapple", "mango", "strawberries", "cherries", "cucumber"];
 
 
 
 for (let i = 0; i < fruit.length; i++) {
 
-console.log (i);
-console.log (fruit);
-
-
- 
-
-
-	
+  console.log(i);
+  console.log(fruit);
 
 }
 
